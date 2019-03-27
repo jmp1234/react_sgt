27e9c651582b1c8efa00b656c8a831e26c4ee4ba
jmp1234 <johnnypham944@yahoo.com>
2019-03-27 12:54:34 -0700

    completed displaying dummy student data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Clark Kent',
+    course: 'Flying 101',
+    grade: '58',
+    id: 1
+  },
+  {
+    name: 'Johnny Pham',
+    course: 'accounting',
+    grade: '18',
+    id: 2
+  },
+  {
+    name: 'Kelly Doe',
+    course: 'economics',
+    grade: '98',
+    id: 3
+  },
+  {
+    name: 'Alex Smith',
+    course: 'Math',
+    grade: '7',
+    id: 4
+  },
+];
